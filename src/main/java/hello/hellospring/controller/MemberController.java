5fe79885bd4af3ff71ebf81ba0c3ea1cc617d2cd
ko-ayeong <alwjd0154@naver.com>
2022-05-06 15:01:11 +0900

    회원 웹 기능 - 조회

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -33,5 +36,16 @@ public class MemberController {
 
         // 회원가입 후 홈화면으로 보냄
         return "redirect:/";
+    }
+
+    // 목록
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        // 모델 안에 키가 멤버스.. 모든 회원을 담아 보낸거임!
+        // 서버를 껐다가 다시키면 데이터가 모두 날아감, 메모리 안에 있기 때문에.. 실무에서 이렇게 하면 큰일!
+        // 그래서 데이터를 데이터베이스나 서버에 저장해야한다
+        model.addAttribute("members", members); // 리스트 자체를 담아서 넘길거임
+        return "members/memberList";
     }
 }
